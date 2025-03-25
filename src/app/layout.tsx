1b16598b2f57751756975ac72a8fc12fab76891f
oyewosolomon <oyewosolomon@gmail.com>
2025-03-25 19:21:08 +0100

    Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
 import { nunitoSans } from './fonts';
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "Africargo: Effortless Logistics, Elevated Performance.",
